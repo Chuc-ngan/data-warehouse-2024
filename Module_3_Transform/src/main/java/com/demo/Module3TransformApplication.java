80844e2970220d712456c39811cf5885b9f36ced
Nhut Tan <21130530@st.hcmuaf.edu.vn>
2024-11-09 14:35:08 +0700

    UPDATE: add main function for testing call proc

--- Module_3_Transform/src/main/java/com/demo/Module3TransformApplication.java
+++ Module_3_Transform/src/main/java/com/demo/Module3TransformApplication.java
@@ -1,13 +1,21 @@
 package com.demo;
 
+import com.demo.services.ProductService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class Module3TransformApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(Module3TransformApplication.class, args);
+        // Chạy Spring Boot application
+        ConfigurableApplicationContext context = SpringApplication.run(Module3TransformApplication.class, args);
+
+        // Lấy ProductService từ Spring context
+        ProductService productService = context.getBean(ProductService.class);
+
+        productService.TransformData();
     }
 
 }
