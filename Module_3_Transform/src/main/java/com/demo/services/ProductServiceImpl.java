5b7e3dd28213b10ff461156550b7388f787c621c
Nhut Tan <21130530@st.hcmuaf.edu.vn>
2024-11-09 14:33:43 +0700

    UPDATE: update file for call procedure

--- Module_3_Transform/src/main/java/com/demo/services/ProductServiceImpl.java
+++ Module_3_Transform/src/main/java/com/demo/services/ProductServiceImpl.java
@@ -4,6 +4,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.Map;
+
 @Service
 public class ProductServiceImpl implements ProductService {
 
@@ -12,9 +15,14 @@ public class ProductServiceImpl implements ProductService {
     @Autowired
     private JdbcTemplate jdbcTemplate; // Kết nối tới database control
 
-
     @Override
     public void TransformData() {
+        // Gọi stored procedure và lấy kết quả
+        List<Map<String, Object>> results = jdbcTemplate.queryForList("CALL transform_and_cleaning_data()");
 
+        // In kết quả ra console
+        for (Map<String, Object> row : results) {
+            System.out.println(row);
+        }
     }
 }
